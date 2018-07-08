e4e5b363383775b49a661be2a842bd80217ac15e
onetoughcookie226 <onetoughcookie226@gmail.com>
2018-07-08 17:37:24 +0200

    Rendering elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import './App.css';
 
 class App extends Component {
@@ -7,8 +7,21 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      jobs: [],
-      users: []
+      jobs: [
+        {
+          "id":1,
+          "title": "",
+          "Company": "",
+          "description": "",
+          "hasApplication": false
+        }
+      ],
+      users: [
+        {
+          "id": 1,
+          "name": "Jonnie walker"
+        }
+      ]
     };
   }
 
@@ -32,20 +45,19 @@ class App extends Component {
                 <div className="col-xs-10">
                     <div className="search-results">
                         <div>
-                          <div className="title"><h3>{this.props.title}</h3></div>
-                          
-                          <small className="text-muted">Gummies muffin International</small>
-                          <p>
-                              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. dolore eu fugiat nulla pariatu.
-                          </p> 
+                          <div className="title"><h3>{this.props.title}</h3></div>       
+                          <small className="text-muted">{this.props.company}</small>
+                          <div className=""><p>{this.props.description}</p> </div>
                           <button type="submit" className="btn btn-default btn-retract">Retract</button>
                         </div>
                     </div>
 
                     <div className="search-results">
                         <div>
-                            <h3>Head Chet</h3>
-                            <small className="text-muted">Lemon drops Amsterdam</small>
+                            {/* <h3>Head Chet</h3> */}
+                            <div className="title"><h3>{this.props.title}</h3></div>
+
+                            <small className="text-muted"></small>
                             <p>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. dolore eu fugiat nulla pariatu.
                             </p> 
@@ -64,6 +76,18 @@ class App extends Component {
 
 
 }
+
+// class Navbar extends React.Component{
+//   constructor(props) {
+//     super(props);
+
+//   }
+
+//   render () {
+//     return
+  
+//   }
+// }
 // App.propTypes= {
 //   title:propTypes.string,
 //   id:propTypes.number,
--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App title="Resturant Manager"
+                // id="1"
+                company= "Gummies Muffin International"
+                description= "Gummies muffin sesame snaps fruitcake. Cookie macaroon macaroon. Ice cream donut apple pie sweet muffin apple pie muffin pudding. Muffin caramels croissant. Jelly cotton candy candy canes gummies tootsie roll halvah. Tootsie roll topping croissant tiramisu topping jelly-o pudding. Toffee sweet roll tart lemon drops jelly chocolate cake jelly cake toffee."
+    />, document.getElementById('root'));
 registerServiceWorker();
